c2a3c82d27cabe8e6c08a0ba8235c763b8f2dca6
OliviaShi <yas089@ucsd.edu>
2019-07-26 11:17:35 +0800

    Add dotenv - Protect my secret data in .env file

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,39 @@
+class DosesController < ApplicationController
+
+  # def show
+  #   @dose = Dose.find(params[:id])
+  # end
+
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredient = Ingredient.all.order(name: :desc)
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.delete
+    redirect_to cocktail_path(@cocktail)
+  end
+
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,72 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# require 'json'
+# require 'open-uri'
+
+Dose.delete_all if Rails.env.development?
+Ingredient.delete_all if Rails.env.development?
+Cocktail.delete_all if Rails.env.development?
+
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# ingredientlist = open(url).read
+# ingredients = JSON.parse(ingredientlist)
+
+
+# # ingredients["drinks"].each do |ingre|
+# #   i = Ingredient.create!(name: ingre["strIngredient1"])
+# # end
+
+
+# # puts "Created #{Ingredient.count} ingredients"
+
+# url = 'https://raw.githubusercontent.com/maltyeva/iba-cocktails/master/recipes.json'
+# cocklist = open(url).read
+# cocktails = JSON.parse(cocklist)
+
+# # cocktails.each do |ingre|
+# #   Cocktail.create!(name: ingre["name"])
+# # end
+
+
+
+# ingredients["drinks"].each do |ingre|
+#   Ingredient.create!(name: ingre["strIngredient1"])
+# end
+
+# cocktails.each do |ingre|
+#   j = Cocktail.create(name: ingre["name"])
+
+#   collection = %w(tea-spoon handful ggfu part)
+#   d = Dose.create(ingredient: Ingredient.all.sample, cocktail: j, description: collection.sample)
+#   # puts "Added #{d.description} of #{d.ingredient.name} to #{j.name}"
+# end
+
+# puts "Created #{Ingredient.count} ingredients"
+# puts "Created #{Dose.count} doses"
+# puts "Created #{Cocktail.count} cocktails"
+
+
+require 'csv'
+
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+
+
+def read(filepath)
+  CSV.new(open(filepath), :headers => :first_row).each do |row|
+    # row = row.gsub!(/\A"|"\Z/, '')
+    c =  Cocktail.find_or_create_by(name: row["name"])
+
+    ingreArray = row["ingredients"].split(",")
+    j = Ingredient.create(name: ingreArray.first)
+    # p Ingredient.create!(name: row["name"])
+    p Dose.create(description: row["toppings"], cocktail: c, ingredient: j )
+  end
+end
+
+read('https://raw.githubusercontent.com/seatuna/bubble-tea-api/master/data/drinks.csv')
+
+puts "Created #{Ingredient.count} ingredients"
+puts "Created #{Dose.count} doses"
+puts "Created #{Cocktail.count} cocktails"
